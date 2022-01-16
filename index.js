bfbfbde36e864067374679ffb75cdd6c3fe26207
Larry Motuzis <larrymotuzis@gmail.com>
2022-01-16 17:08:24 -0600

    commit tree 3af65e772af47dbb275d7eba498873da247d2ccb

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+var customerName = "bob"
+const leastFavoritecustomer = 'tim'
+
+
+function upperCaseCustomerName() {
+    return customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+    return function(){
+        bestCustomer = 'not bob'
+    }
+}
+
+console.log(setBestCustomer())
+
+var overwriteBestCustomer = () => bestCustomer = 'maybe bob'
+console.log(overwriteBestCustomer())
+
